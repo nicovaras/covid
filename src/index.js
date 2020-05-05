b1928bfb3c83a61a243465ab85905a831a316652
Nico Varaschin <nicovaras22@gmail.com>
2020-05-05 12:03:14 -0300

    Google analytics

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Dashboard from './Dashboard';
 
-
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-92772547-2');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 // ========================================
 
