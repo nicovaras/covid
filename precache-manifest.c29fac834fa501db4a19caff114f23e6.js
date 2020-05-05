950658ced74a879e07b1a5c328cd0cb9e85cafcd
Nico Varaschin <nicovaras22@gmail.com>
2020-05-05 13:17:10 -0300

    Updates

---
+++ precache-manifest.c29fac834fa501db4a19caff114f23e6.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a743b7e1124248159c00c02097477e48",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "83ac60871e3323705bc3",
+    "url": "/covid/static/css/2.1481cc13.chunk.css"
+  },
+  {
+    "revision": "0fa8d072248a77f168bc",
+    "url": "/covid/static/css/main.3f269fe0.chunk.css"
+  },
+  {
+    "revision": "83ac60871e3323705bc3",
+    "url": "/covid/static/js/2.9ac75c98.chunk.js"
+  },
+  {
+    "revision": "6b9c301859d114de765abc786b4c4b51",
+    "url": "/covid/static/js/2.9ac75c98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa8d072248a77f168bc",
+    "url": "/covid/static/js/main.dc8f179d.chunk.js"
+  },
+  {
+    "revision": "ad17bd053aeacfad6e01",
+    "url": "/covid/static/js/runtime-main.26305e11.js"
+  },
+  {
+    "revision": "4c3bc2939dfa17434560d174cf669f98",
+    "url": "/covid/static/media/BuenosAires.4c3bc293.png"
+  },
+  {
+    "revision": "fcd7c66b7081a6ae968e65e36a1bd7e2",
+    "url": "/covid/static/media/CABA.fcd7c66b.png"
+  },
+  {
+    "revision": "a3ffc25d8e287513716029c7c6c632a6",
+    "url": "/covid/static/media/Catamarca.a3ffc25d.png"
+  },
+  {
+    "revision": "daef53217f4415aacb82e852176d7ee6",
+    "url": "/covid/static/media/Chaco.daef5321.png"
+  },
+  {
+    "revision": "cd9fe698dcc669fa31284ca5169b1101",
+    "url": "/covid/static/media/Chubut.cd9fe698.png"
+  },
+  {
+    "revision": "7d6e533628415fdcf4bbf0b736bf21dd",
+    "url": "/covid/static/media/Cordoba.7d6e5336.png"
+  },
+  {
+    "revision": "5003f88203c0322c7e5943316eef66c2",
+    "url": "/covid/static/media/Corrientes.5003f882.png"
+  },
+  {
+    "revision": "0d1e7d252381b43e7a8ba0f4b66090db",
+    "url": "/covid/static/media/EntreRios.0d1e7d25.png"
+  },
+  {
+    "revision": "890cdda5dcf500fceccde9e145df69e5",
+    "url": "/covid/static/media/Formosa.890cdda5.png"
+  },
+  {
+    "revision": "20405a6e3ac38e243c9833ee5545ecaa",
+    "url": "/covid/static/media/Jujuy.20405a6e.png"
+  },
+  {
+    "revision": "2b78f1013fd899b2f33022ff0339bdde",
+    "url": "/covid/static/media/LaPampa.2b78f101.png"
+  },
+  {
+    "revision": "711ba9d9224650245e2141b0afd9aae7",
+    "url": "/covid/static/media/LaRioja.711ba9d9.png"
+  },
+  {
+    "revision": "185a262b351371a48475ece199c4ed68",
+    "url": "/covid/static/media/Mendoza.185a262b.png"
+  },
+  {
+    "revision": "697141a667b0f0b70e62744ac49f8ba4",
+    "url": "/covid/static/media/Misiones.697141a6.png"
+  },
+  {
+    "revision": "3e711c3ed7357baf7e1a5fb9e41b1957",
+    "url": "/covid/static/media/Neuquen.3e711c3e.png"
+  },
+  {
+    "revision": "865ffdfba68ab891ffb38dd1a3897a0c",
+    "url": "/covid/static/media/RioNegro.865ffdfb.png"
+  },
+  {
+    "revision": "645a67ab02a5b9e9c57575b299f99f05",
+    "url": "/covid/static/media/Salta.645a67ab.png"
+  },
+  {
+    "revision": "b6301f22f524a36d8d5b92220b7a8f20",
+    "url": "/covid/static/media/SanJuan.b6301f22.png"
+  },
+  {
+    "revision": "c3d46a4d9507e8b2435b11536f7abe6b",
+    "url": "/covid/static/media/SanLuis.c3d46a4d.png"
+  },
+  {
+    "revision": "df49222b033707fb0b159c2fcf1cb4bc",
+    "url": "/covid/static/media/SantaCruz.df49222b.png"
+  },
+  {
+    "revision": "29ff897338717cb5d5c263aaca524479",
+    "url": "/covid/static/media/SantaFe.29ff8973.png"
+  },
+  {
+    "revision": "bc77d25504f3036f6d450f09b1658e0f",
+    "url": "/covid/static/media/SantiagodelEstero.bc77d255.png"
+  },
+  {
+    "revision": "cdf62d47fcaef3e8057d485b748948cb",
+    "url": "/covid/static/media/TierradelFuego.cdf62d47.png"
+  },
+  {
+    "revision": "db5372752124830fcb829e387a0fc011",
+    "url": "/covid/static/media/Tucuman.db537275.png"
+  }
+]);
